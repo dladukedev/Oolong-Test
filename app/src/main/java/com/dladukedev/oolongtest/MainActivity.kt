77db3e2fe2c6516dca69279f4bd4f24142405f28
Donovan LaDuke <dladukedev@gmail.com>
2020-03-05 20:27:42 -0500

    updated compose and kotlin versions

--- app/src/main/java/com/dladukedev/oolongtest/MainActivity.kt
+++ app/src/main/java/com/dladukedev/oolongtest/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
-import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 import com.dladukedev.oolongtest.Store.init
 import com.dladukedev.oolongtest.Store.update
 import com.dladukedev.oolongtest.Store.view
@@ -39,16 +38,14 @@ class MainActivity : AppCompatActivity() {
                 setContent {
                     MaterialTheme {
                         Center {
-                            Column(crossAxisAlignment = CrossAxisAlignment.Stretch) {
-                                Row(mainAxisAlignment = MainAxisAlignment.Center) {
-                                    DisplayResults(props)
-                                }
-                                HeightSpacer(10.dp)
-                                Row(mainAxisAlignment = MainAxisAlignment.Center) {
-                                    Button(
-                                        text = "Get Data",
-                                        onClick = { dispatch(props.getData()) }
-                                    )
+                            Column {
+                                DisplayResults(props)
+                                Spacer(modifier = LayoutHeight(10.dp))
+                                Button(
+                                    modifier = LayoutGravity.Center,
+                                    onClick = { dispatch(props.getData()) }
+                                ) {
+                                    Text("Get Data")
                                 }
                             }
                         }
